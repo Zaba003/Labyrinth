77d3159605821d398c40736febc037a9e43684bd
Kirill Zaborsky <avto.oil@gmail.com>
2021-10-14 11:03:18 +0700

    Initial Commit

---
+++ Kristall&Dragon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kristall&Dragon
+//
+//  Created by Кирилл Заборский on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
